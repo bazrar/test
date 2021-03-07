888b5c9693a8507463c33c67bc40a59b14208967
varbuzz <buzz.rejan@gmail.com>
2021-03-07 16:23:05 +0545

    use async await

--- api-call.js
+++ api-call.js
@@ -5,13 +5,27 @@ const server = require('http').createServer();
 
 server.listen(8000,()=>console.log('listening ...'))
 server.on('request', (req, res) => {
-     fs.readFile(`${__dirname}/dogs.txt`, 'utf-8', (err, data) => {
-         if(err) return res.end(err)
-        axios.get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.data)
-          fs.writeFile(`${__dirname}/dog-img.txt`, res.data.message, err => console.log(err))
-        })
-        .catch(err => console.log(err))
+      fs.readFile(`${__dirname}/dogs.txt`, 'utf-8', async (err, data) => {
+
+        // use promise
+        //  if(err) return res.end(err)
+        // axios.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        // .then(res => {
+        //     console.log(res.data)
+        //   fs.writeFile(`${__dirname}/dog-img.txt`, res.data.message, err => console.log(err))
+        // })
+        // .catch(err => console.log(err))
+
+        // use async/await
+        // console.log(data)
+        try {
+            const res = await  axios.get(`https://dog.ceo/api/breed/${data}/images/random`)
+            // console.log(res)
+        fs.writeFile(`${__dirname}/dog-img.txt`, res.data.message, err => console.log(err))
+        
+        }
+        catch(err) {
+            console.log(err)
+        }
     })
 })
